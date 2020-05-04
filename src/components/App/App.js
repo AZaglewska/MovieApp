5921a5bfe24a42cf7017931db13ac6528d139898
Anna Żaglewska <aa.zaglewska@gmail.com>
2020-05-04 12:31:00 +0200

    movies

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>u mnie działa</h1>
+    </div>
+  );
+}
+
+export default App;
